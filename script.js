c3f15fdd48a1ffd1e7f89a35f6643071403c792a
Shaik Fathima <141469226+SkFathima@users.noreply.github.com>
2023-08-27 15:24:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const ctaButton = document.querySelector(".cta-button");
-
-  ctaButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    const targetSection = document.querySelector(this.getAttribute("href"));
-    targetSection.scrollIntoView({ behavior: "smooth" });
-  });
-});
